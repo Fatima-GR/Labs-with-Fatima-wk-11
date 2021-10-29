b3940134a877b813e1a59cf944790adbb87905c6
Kara Howes <karahowes@Karas-MacBook-Air.local>
2021-10-29 11:04:59 +0200

    finished styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import Header from './components/Header'
 
+
  const App = () => {
   return (
-    <>
+    <div className="header">
       <Header 
-      headerText ='Get Ready for Adventure'
-      textColor = 'orange'
-      backgroundColor='black'
+      headerText ='Get ready for adventure!'
+      headerColor = 'white'
+      backgroundColor='#151414'
       circleColor ='#DD8723'
+      navColor='#FFF5D1'
       a='about'
       b='trips'
       c='pricing'
       d='contact'
       />
-    </>
+    </div>
   )
 }
  export default App
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,29 @@
 import React from 'react'
 
-const Header = (props) => {
+const Header = ({headerText,headerColor,backgroundColor,circleColor,navColor,a,b,c,d}) => {
 
     const styles = {
-        backgroundColor:props.backgroundColor,
-        circleColor:props.circleColor,
-        textColor:props.headerText
+        backgroundColor: backgroundColor,
+        circleColor: circleColor,
+        headerText: headerText,
+        headerColor: headerColor
     }
     return (
-    <div style={styles}>
-        
-       <div>
-            {props.backgroundColor}
-       </div>
-        <nav>
-            <a>{props.a}</a>
-            <a>{props.b}</a>
-            <a>{props.c}</a>
-            <a>{props.d}</a>
+    <header className="header" style={styles}>
+        <nav style={{color:navColor}} className="nav">
+            <a>{a}</a>
+            <a>{b}</a>
+            <a>{c}</a>
+            <a>{d}</a>
         </nav>
-    </div>
+        <h1 style={{ color: headerColor}} className="heading-one">{headerText}</h1>
+        
+       <div className="circle">
+           <div style ={{backgroundColor:circleColor}} className="circle"></div>
+            
+       </div>
+        
+    </header>
     )
 }
 
